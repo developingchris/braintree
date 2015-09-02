f1a5aa2721f97e6aff5701a5b998d6d28c84037e
Luiza Libardi <luizamcf@gmail.com>
2015-09-02 14:07:56 -0700

    NX-849 .: Update Documentation in All Plugins

--- docs/Braintree Payments/js/documentation.js
+++ docs/Braintree Payments/js/documentation.js
@@ -1,19 +1,51 @@
 jQuery(document).ready(function() {
   // LICENSE
   // hide documentation unless license terms are agreed
-  jQuery('#licenseAgreed').hide();
+  //jQuery('#licenseAgreed').hide();
   // show the documentation if the page loads with agreement radio checked
-  if (jQuery('input[name="license"]:checked').val() == 1) {
-    jQuery('#licenseAgreed').fadeIn();
-  }
+  // if (jQuery('input[name="license"]:checked').val() == 1) {
+  //   jQuery('#licenseAgreed').fadeIn();
+  // }
   // bind click event to license agreement and show/hide documentation based on selection
-  jQuery('input[name="license"]').live('click', function() {
-    if (jQuery('input[name="license"]:checked').val() == 1) {
-      jQuery('#licenseAgreed').fadeIn(); 
-    } else {
-      jQuery('#licenseAgreed').fadeOut();
-    } 
+  // jQuery('input[name="license"]').live('click', function() {
+  //   if (jQuery('input[name="license"]:checked').val() == 1) {
+  //     jQuery('#licenseAgreed').fadeIn(); 
+  //   } else {
+  //     jQuery('#licenseAgreed').fadeOut();
+  //   }
+  // });
+
+// Menu Links
+  $("#btnInstallation").click(function(){
+    $(".bodyHeaderContainer").hide();
+    $("#Installation").show();
   });
+   $("#btnInstallationTips").click(function(){
+    $(".bodyHeaderContainer").hide();
+    $("#InstallationTips").show();
+  });
+    $("#btnUninstall").click(function(){
+    $(".bodyHeaderContainer").hide();
+    $("#Uninstall").show();
+  });
+    $("#btnAbout").click(function(){
+    $(".bodyHeaderContainer").hide();
+    $("#About").show();
+  });
+    $("#btnLicense").click(function(){
+    $(".bodyHeaderContainer").hide();
+    $("#License").show();
+  });
+    $("#btnDisclaimer").click(function(){
+    $(".bodyHeaderContainer").hide();
+    $("#Disclaimer").show();
+  });
+    $("#btnHelp").click(function(){
+    $(".bodyHeaderContainer").hide();
+    $("#Help").show();
+  });
+
+
   
   // CODE BOXES
   // highlight
@@ -69,4 +101,5 @@ function selectCode(a)
     r.moveToElementText(e);
     r.select();
   }
+
 }
